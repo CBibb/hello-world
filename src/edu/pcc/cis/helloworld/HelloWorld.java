4a8e04086044a616d6c36f570117f44dadd86be8
Christian Tysoe <christian.tysoe@pcc.edu>
2021-10-24 01:37:47 -0700

    created main method; prints a message

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,15 @@
 package edu.pcc.cis.helloworld;
 
+
+/**
+ * A simple hello world project,
+ * With a few variations.
+ * @authors Christian Tysoe
+ */
+
 public class HelloWorld {
+    public static void main (String[] args)
+    {
+        System.out.println("If you can see this, then the program is working.");
+    }
 }
